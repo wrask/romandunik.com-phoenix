5467a4db70d531055ef5a87e7a53b0d45d0e6bf4
Roman Dunik <wrask7@gmail.com>
2025-09-06 10:39:42 +0300

    Small updates

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
+  alias Hello.Blog
+
   defp common_variables do
     %{
       elixir_version: System.version(),
@@ -13,8 +15,8 @@ defmodule HelloWeb.PageController do
   end
 
   def index(conn, _params) do
-    posts = Hello.Blog.list_posts()
-    view_counts = Hello.Blog.get_post_view_counts(Enum.map(posts, & &1.id))
+    posts = Blog.list_posts()
+    view_counts = Blog.get_post_view_counts(Enum.map(posts, & &1.id))
 
     conn
     |> render(:index,
@@ -25,10 +27,10 @@ defmodule HelloWeb.PageController do
   end
 
   def show(conn, %{"slug" => slug}) do
-    post = Hello.Blog.get_post_by_id!(slug)
+    post = Blog.get_post_by_id!(slug)
 
     Hello.Blog.increment_post_view!(post.id)
-    view_count = Hello.Blog.get_post_view_count(post.id)
+    view_count = Blog.get_post_view_count(post.id)
 
     conn
     |> render(:show,
@@ -39,8 +41,8 @@ defmodule HelloWeb.PageController do
   end
 
   def tags(conn, %{"tag" => tag}) do
-    posts = Hello.Blog.list_posts_by_tag!(tag)
-    view_counts = Hello.Blog.get_post_view_counts(Enum.map(posts, & &1.id))
+    posts = Blog.list_posts_by_tag!(tag)
+    view_counts = Blog.get_post_view_counts(Enum.map(posts, & &1.id))
 
     conn
     |> render(:index,
--- posts/2025/08-22-first-post.md
+++ posts/2025/08-22-first-post.md
@@ -11,21 +11,4 @@
 
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
-## Code Examples
-
-Here's some Elixir code with syntax highlighting:
-
-```elixir
-defmodule Hello.Blog do
-  use NimblePublisher,
-    build: Post,
-    from: "./posts/**/*.md",
-    as: :posts
-
-  @posts Enum.sort_by(@posts, & &1.date, {:desc, Date})
-  
-  def list_posts, do: @posts
-end
-```
-
 
