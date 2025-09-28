b83de9a3eacfa348d3a52e6609a034b88ff986f4
Roman Dunik <wrask7@gmail.com>
2025-09-28 12:51:40 +0300

    Update packages

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule Hello.MixProject do
       {:phoenix_html, "4.2.1"},
       {:phoenix_live_dashboard, "0.8.7"},
       {:phoenix_live_reload, "1.6.1", only: :dev},
-      {:phoenix_live_view, "1.1.12"},
+      {:phoenix_live_view, "1.1.13"},
       {:postgrex, "0.21.1"},
       {:telemetry_metrics, "1.1.0"},
       {:telemetry_poller, "1.3.0"}
